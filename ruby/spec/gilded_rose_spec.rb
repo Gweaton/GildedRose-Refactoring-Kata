de9397cd7578f58975771f6b9453b799202bef41
George Eaton <george_eaton@hotmail.co.uk>
2017-01-25 11:17:48 +0000

    add test to check sell_in date is reduced by 1 each time update_quality is run

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -48,5 +48,14 @@ describe GildedRose do
         expect(items[0].quality).to eq 50
       end
     end
+
+    context 'sell_in date' do
+      it "should reduce sell_in date of normal items by 1 each time" do
+        items = [Item.new("item", 2, 2)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].sell_in).to eq 1
+      end
+    end
+
   end
 end
