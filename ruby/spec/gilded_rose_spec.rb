7fb233d93d968a04aeb968fa09a407ef2ea5855d
George Eaton <george_eaton@hotmail.co.uk>
2017-01-25 11:38:34 +0000

    add test to check backstage pass value increases by 2 10 days before sell_in date

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -54,6 +54,11 @@ describe GildedRose do
           GildedRose.new(items).update_quality()
           expect(items[0].quality).to eq 3
         end
+        it "should increase quality by 2 when 10 days from sell_in date" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 2)]
+          GildedRose.new(items).update_quality()
+          expect(items[0].quality).to eq 4
+        end
       end
     end
 
