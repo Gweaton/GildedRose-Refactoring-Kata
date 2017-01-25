3d6b21d746f079abc916ed5c1871c9a9990b5f7c
George Eaton <george_eaton@hotmail.co.uk>
2017-01-25 12:12:32 +0000

    refactors update quality brie to increase quality

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -18,12 +18,12 @@ class GildedRose
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
-                item.quality = item.quality + 1
+                increase_quality(item)
               end
             end
             if item.sell_in < 6
               if item.quality < 50
-                item.quality = item.quality + 1
+                increase_quality(item)
               end
             end
           end
@@ -37,26 +37,30 @@ class GildedRose
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
+                decrease_normal_item_quality(item)
               end
             end
           else
             item.quality = item.quality - item.quality
           end
         else
-          update_quality_brie(item)
+          increase_quality(item)
         end
       end
     end
   end
 end
 
-def update_quality_brie(item)
+def decrease_sell_in_date(item)
+  item.sell_in -= 1
+end
+
+def increase_quality(item)
   item.quality += 1 if item.quality < 50
 end
 
-def decrease_sell_in_date(item)
-  item.sell_in -= 1
+def decrease_normal_item_quality(item)
+  item.quality < 0 ? item.quality -= 2 : item.quality -= 1
 end
 
 
