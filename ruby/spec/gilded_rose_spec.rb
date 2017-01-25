ff4617843091233214cb928e47015720b1ba1592
George Eaton <george_eaton@hotmail.co.uk>
2017-01-25 10:53:49 +0000

    add test to check quality is reduced by 1 each time update_quality is run

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -3,11 +3,20 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
+
+    before do
+    end
+
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
-  end
 
+    it "should reduce quality of normal items by 1 each time" do
+      items = [Item.new("item", 2, 2)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 1
+    end
+  end
 end
